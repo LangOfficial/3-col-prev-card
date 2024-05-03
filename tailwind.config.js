7c2d04758d8e5d78fdd54b2cc0f39ff5bdf79e5e
Langli <Langliland64@gmail.com>
2024-05-02 18:45:35 -0700

    completed file setup

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,23 @@ export default {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: {
+          400: 'hsl(31, 77%, 52%)',
+          600: 'hsl(184, 100%, 22%)',
+          700: 'hsl(179, 100%, 13%)',
+        },
+        neutralC: {
+          0: 'hsla(0, 0%, 100%, 0.75)',
+          100: 'hsl(0, 0%, 95%)',
+        },
+      },
+      fontFamily: {
+        primary: 'Lexend Deca',
+        secondary: 'Big Shoulders Display'
+      },
+    },
   },
   plugins: [],
 }
